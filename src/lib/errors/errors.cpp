cf1db8f92fdb45cf7f84bc440048977119cd2dac
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2026-03-15 11:00:10 +0300

    Обновлён код для обработки ошибок

--- src/lib/errors/errors.cpp
+++ src/lib/errors/errors.cpp
@@ -1,1 +1,10 @@
 #include "errors.h"
+
+namespace errors {
+
+RuntimeError::RuntimeError(const std::string& message, const std::string& file, const size_t line)
+    : std::runtime_error(utils::FormatStream() << "An exception has occurred.\n"
+                                               << "File " << file << ":" << line << ".\n"
+                                               << message) {}
+
+}  // namespace errors
--- src/lib/errors/errors.h
+++ src/lib/errors/errors.h
@@ -1,16 +1,28 @@
 #pragma once
 
+#include <exception>
 #include <stdexcept>
 
 #include "../utils/format_stream.h"
 
+#define THROW(error_class, format_expr) \
+    throw error_class(utils::FormatStream() << format_expr, __FILE__, __LINE__)
+#define THROW_IF(condition, error_class, format_expr) \
+    if (condition) {                                  \
+        THROW(error_class, format_expr);              \
+    }                                                 \
+    (void)0
+
 namespace errors {
 
-#define THROW(error_class, format_expr) throw (error_class)(utils::FormatStream() << format_expr)
-
-class LogicError : public std::logic_error {
+class RuntimeError : public std::runtime_error {
 public:
-    using std::logic_error::logic_error;
+    RuntimeError(const std::string& message, const std::string& file, const size_t line);
 };
 
-}
+class LogicError : public RuntimeError {
+public:
+    using RuntimeError::RuntimeError;
+};
+
+}  // namespace errors
