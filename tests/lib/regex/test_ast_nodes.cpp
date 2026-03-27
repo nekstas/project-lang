c099d42411a2d95101aecd1c69c72201699bd1f1
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2026-03-27 17:51:01 +0300

    Проведён небольшой рефакторинг

--- tests/lib/regex/test_ast_nodes.cpp
+++ tests/lib/regex/test_ast_nodes.cpp
@@ -2,15 +2,52 @@
 
 #include "../../../src/lib/errors/errors.h"
 #include "../../../src/lib/regex/ast/all.h"
-#include "utils.h"
 
 namespace {
 
+using lang::regex::ast::Node;
 using lang::regex::ast::CharNode;
 using lang::regex::ast::ChoiceNode;
 using lang::regex::ast::RepeatNode;
 using lang::regex::ast::RepeatType;
 using lang::regex::ast::SequenceNode;
+
+class AstBuilder {
+private:
+    using NodesVector = std::vector<std::unique_ptr<Node>>;
+
+public:
+    AstBuilder& AddNode(std::unique_ptr<Node> node) {
+        nodes_.emplace_back(std::move(node));
+        return *this;
+    }
+
+    template <typename T, typename... Args>
+    AstBuilder& Add(Args&&... args) {
+        return AddNode(std::make_unique<T>(std::forward<Args>(args)...));
+    }
+
+    AstBuilder& AddChar(uint8_t code) {
+        return Add<CharNode>(code);
+    }
+
+    AstBuilder& AddChars(const std::string& str) {
+        for (const auto code : str) {
+            AddChar(code);
+        }
+        return *this;
+    }
+
+    template <typename T>
+    std::unique_ptr<T> Make() {
+        auto result = std::make_unique<T>(std::move(nodes_));
+        nodes_ = NodesVector{};
+        return std::move(result);
+    }
+
+private:
+    NodesVector nodes_;
+};
 
 TEST_CASE("lang::regex::ast nodes") {
     SECTION("lang::regex::ast::CharNode") {
