ea9567302361ec95ac103b83b78add2a78304463
Станислав Некрасов <77635076+nekstas@users.noreply.github.com>
2026-03-30 00:49:50 +0300

    Добавлена временная заготовка для функции main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
+#include "lib/flow/static_pipeline.hpp"
+#include "lib/lang/context/base_context.h"
+#include "lib/lang/frontend/read_file/read_file.hpp"
+
+struct Context : lib::lang::BaseContext {};
+
 int main() {
+    auto pipeline = lib::flow::MakeStaticNamedPipeline<Context>(
+        "LangPipeline", lib::lang::frontend::ReadFileStage<Context>{});
+
     std::cout << "Hello, project-lang!\n";
+    std::cout << pipeline.Name() << "\n";
+
+    Context ctx;
+    try {
+        auto result = pipeline.Run("../../example.lang", ctx);
+        std::cout << result;
+    } catch (const lib::flow::StopPipeline& stop_pipeline) {
+        std::cerr << "Pipeline stopped: " << stop_pipeline.GetLocation() << "\n";
+    }
+    std::cerr << ctx.diag.GetDiagnosticsString();
 }
